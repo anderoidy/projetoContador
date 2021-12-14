a6d36dfe63d1110a0670b841b3324c41e92aaec5
anderoidy <anderoidy@gmail.com>
2021-12-14 03:17:53 -0300

    first commit

---
+++ assets/JS/FuncaoNumerosPares.js
@@ -0,0 +1,23 @@
+let listaNumeros = [1, 3, 4, 5, 6, 8, 9, 12, 13, 16, 20, 21, 22, 23, 24, 30, 50, 1080, 1081, 1083]
+//imprimindo a lista toda
+console.log(listaNumeros)
+
+//funcao apenas os pares
+listaNumeros.forEach(function(pares) {    
+    if(pares % 2 === 0) {
+        //imprimindo apenas os pares
+        console.log("Numero " +pares+ " é par")      
+    }    
+})
+
+
+
+ 
+
+
+
+
+
+
+
+
